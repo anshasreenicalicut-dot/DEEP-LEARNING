6f10e0f4b131a361c4728b6283174539f96fb444
Ansha T V <anshasreenicalicut@gmail.com>
2026-03-31 16:16:13 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,35 +1,23 @@
 import streamlit as st
-import numpy as np
-import tensorflow as tf
+from ultralytics import YOLO
 from PIL import Image
-import json
+import numpy as np
 
-# Load model
-model = tf.keras.models.load_model('image_classification_model.h5', compile=False)
+st.title("Object Detection App")
+st.write("Upload an image for detection")
 
-# Load class names
-with open("class_names.json", "r") as f:
-    class_names = json.load(f)
+# load trained model
+model = YOLO("C:\\Users\\Ansha TV\\Desktop\\Data Science\\Deep Learning\\deep learning\\runs\\detect\\train32\\weights\\best.pt")
 
-st.title("🚢 Ship Image Classification")
-
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg","jpeg","png"])
+uploaded_file = st.file_uploader("Choose an image", type=["jpg","png","jpeg"])
 
 if uploaded_file is not None:
-    # Convert to RGB
-    image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded Image", use_container_width=True)
+    
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
 
-    # Preprocess
-    img = image.resize((128, 128))
-    img_array = np.array(img) / 255.0
-    img_array = np.expand_dims(img_array, axis=0)  # shape (1,128,128,3)
+    if st.button("Detect Objects"):
+        results = model(image)
+        result_image = results[0].plot()
 
-    # Predict
-    prediction = model.predict(img_array)
-    predicted_index = np.argmax(prediction)
-    predicted_label = class_names[predicted_index]
-    confidence = np.max(prediction) * 100
-
-    st.success(f"Predicted Ship Type: {predicted_label}")
-    st.info(f"Confidence: {confidence:.2f}%")
+        st.image(result_image, caption="Detection Result", use_container_width=True)
